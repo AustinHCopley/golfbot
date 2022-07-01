9e528847edba53a77c2786ac749472a6c1645611
Austin Copley <acopley@mines.edu>
2022-06-30 20:07:54 -0500

    general fixes, refactoring, arranging

--- golfbot.py
+++ golfbot.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-from ctypes.wintypes import HRSRC
 import time
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
-
 from selenium import webdriver
+
 
 class GolfBot():
     """describes the framework of the automated system"""
@@ -18,9 +17,8 @@ class GolfBot():
         
 
     @classmethod
-    def countdown(cls):
+    def countdown(cls, now):
 
-        now = time.ctime()[11:19]
         if int(now[:2]) == hr:
             if int(now[3:5]) == min:
                 print("T- " +
@@ -35,12 +33,6 @@ class GolfBot():
                 str(min - int(now[3:5])) + " min " +
                 str(sec - int(now[6:8])) + " sec"  )
 
-        # ---> stop counting a couple seconds before
-        
-
-    # otherwise, sleep 1 sec
-    time.sleep(1)
-
 
 
 class WebPage():
@@ -51,22 +43,27 @@ class WebPage():
         self.driver = webdriver.Chrome("./chromedriver") # chromedriver executable needs to be in the parent directory
     
     def openPage(self):
-        self.drive.get(self.url)
+        self.driver.get(self.url)
 
-
+##  <<== SELENIUM ==>>
 # TODO -!--> move to after the countdown
+# TODO -!--> connect to website
 url = "https://www.roccdallas.com/"
 rocc = WebPage(url)
 rocc.openPage()
 
+# TODO -!--> maybe login?
+#           \--> input fields?
+#            print("User logged in")
 
-# testing with beautifulsoup
-client = urlopen(url)
+
+# TODO -!--> web scraper with beautifulsoup?
+"""client = urlopen(url)
 html = client.read()
 client.close()
 # parse html
 page = soup(html, "html.parser")
-print(page.title) # test that the page title prints correctly
+print(page.title) # test that the page title prints correctly"""
 
 
 hr = 7
@@ -80,10 +77,16 @@ golf = GolfBot(hr, min, sec, opentime)
 
 # ---> give user time reference
 while now != opentime:
-    golf.countdown()
+
+    golf.countdown(now)
+
+    # ---> stop counting a couple seconds before opentime
     now = time.ctime()[11:19]
     if ( int(now[:2]) == golf.hr ) and ( int(now[3:5]) == golf.min) and ( int(now[6:]) >= golf.sec - 3):
-            break
+        break
+            
+    # otherwise, sleep 1 sec
+    time.sleep(1)
 
 
 # ---> wait until it is 8:00 am
@@ -93,12 +96,9 @@ while time.ctime()[11:19] != opentime:
 print("golf")
 print(time.ctime()[11:19])
 
-# TODO -!--> connect to website
-# TODO -!--> maybe login?
-#           \--> input fields?
-#            print("User logged in")
-# TODO -!--> web scraper?
-# TODO -!--> click position/button?
+
+
+# TODO -!--> click button?
 
 if __name__ == "__main__":
     pass
