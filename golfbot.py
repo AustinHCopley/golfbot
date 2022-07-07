f7b6a59acc5b0f026e18196899f84c83421a133f
Austin Copley <acopley@mines.edu>
2022-07-07 03:09:38 -0500

    tweaks and improvements

--- golfbot.py
+++ golfbot.py
@@ -17,37 +17,52 @@ class GolfBot():
         
     def countdown(self, now):
         # TODO -!--> refactor to reduce redundancy
-        if int(now[:2]) == self.hr:
-            if int(now[3:5]) == min:
-                print("T- " +
-                    str(self.sec - int(now[6:8])) + " sec")
+        # only print every 5 seconds
+        if not int(now[6:8]) % 5:
+            if int(now[:2]) == self.hr:
+                if int(now[3:5]) == min:
+                    print("T- " +
+                        str(self.sec - int(now[6:8])) + " sec")
+                else:
+                    print("T- " +
+                        str(self.min - int(now[3:5])) + " min " +
+                        str(self.sec - int(now[6:8])) + " sec"  )
             else:
                 print("T- " +
+                    str(self.hr  - int(now[ :2])) + " hr "  +
                     str(self.min - int(now[3:5])) + " min " +
                     str(self.sec - int(now[6:8])) + " sec"  )
-        else:
-            print("T- " +
-                str(self.hr  - int(now[ :2])) + " hr "  +
-                str(self.min - int(now[3:5])) + " min " +
-                str(self.sec - int(now[6:8])) + " sec"  )
     
-    def findTimes(self, webPage, h, m):
+    def findTimes(self, webPage, webPage2, h, m):
         # loop through all possible timeslots, and determine if it can be clicked
         meridian = "A"
         while m < 51:
-            if meridian == "P":
+            if meridian == "P": # stop searching for times past 6pm
                 if h > 5:
                     break
-            if h > 12:
+            if h > 12: # switch from AM to PM
                 h = 1
                 meridian = "P"
             
-            if not m:
+            if not m: # if m is 0, make it a string with a leading 0 to fit time format
                 m = "00"
-            if webPage.findLink(f"{h}:{m} {meridian}M"):
-                webPage.clickLink(f"{h}:{m} {meridian}M")
-                print(f"selected {h}:{m} {meridian}M")
-                break
+            # TODO -!--> not a big problem, but the stop bool does not work as expected, and all times are clicked
+            switch = True
+            stop = False
+            if switch: # every other time, switch between the two webpages
+                if webPage.findLink(f"{h}:{m} {meridian}M"):
+                    webPage.clickLink(f"{h}:{m} {meridian}M")
+                    print(f"selected {h}:{m} {meridian}M")
+                    if stop: break # ensure both pages click a time before breaking the loop
+                    else: stop = True
+                switch = False
+            else:
+                if webPage2.findLink(f"{h}:{m} {meridian}M"):
+                    webPage2.clickLink(f"{h}:{m} {meridian}M")
+                    print(f"selected {h}:{m} {meridian}M")
+                    if stop: break
+                    else: stop = True
+                switch = True
             if not isinstance(m, int):
                 m = 0
             m += 10
@@ -83,7 +98,7 @@ class WebPage():
     def __init__(self, url, browser="safari"):
         self.url = url
         if browser == "safari":
-            self.driver = webdriver.Safari("/usr/bin/safaridriver") # safari executable is found in usr/bin
+            self.driver = webdriver.Safari(executable_path="/usr/bin/safaridriver") # safari executable is found in usr/bin
         else:
             self.driver = webdriver.Chrome("./chromedriver") # chromedriver executable needs to be in the parent directory
 
@@ -111,10 +126,8 @@ class WebPage():
             time.sleep(0.5)
             self.clickLink(name)
 
-    def loginButton(self):
-        url = "./MemberIdentification.html"
-        #url = "/SiteDesign/ForeTeesScript.aspx"
-        self.driver.find_element(webdriver.common.by.By.XPATH,f'//a[contains(@href,"{url}")]').click()
+    def loginButton(self, href):
+        self.driver.find_element(webdriver.common.by.By.XPATH,f'//a[contains(@href,"{href}")]').click()
 
     def setURL(self, url):
         self.url = url
@@ -129,25 +142,40 @@ def main():
     date = input("Please enter the day of the month to schedule (e.g. 16): ")
 
     cwd = os.path.abspath(os.getcwd())
-    url = f"file://{cwd}/MemberIdentification.html"
+    url = f"file://{cwd}/GolfLogin.html"
+    #url = ""
     pageA = WebPage(url, "chrome")
     pageA.openPage()
+    pageB = WebPage(url, "chrome")
+    pageB.openPage()
+    time.sleep(1)
 
-    # TODO -!--> move the countdown here once testing is done, or move it after a refresh calendar click
-    # TODO -!--> login to renew session
+    # login to renew session
+
+    href = "./MemberIdentification.html"
+    #href = "/SiteDesign/ForeTeesScript.aspx"
+    pageA.loginButton(href)
+    pageB.loginButton(href)
+    time.sleep(1)
 
     # click austin
-    # all buttons are <a> tags
-    time.sleep(1)
+    # all buttons onward are <a> tags
     pageA.clickLink("Austin Copley")
-    # click on the inputted date
+    pageB.clickLink("Member 1")
     time.sleep(1)
+
+    # TODO -!--> move the countdown here once testing is done, or move it after a refresh calendar click
+    
+
+    # click on the inputted date
+
     pageA.clickLink(date)
+    pageB.clickLink(date)
     time.sleep(1)
 
     # pass the webpage to the golfbot instance to locate tee times
     h, m = int(teetime.split(":")[0]), int(teetime.split(":")[1])
-    golf.findTimes(pageA, h, m)
+    golf.findTimes(pageA, pageB, h, m)
 
     # TODO -!--> either give user opportunity to select their own players and methods of transport
     #            or autofill fields, or leave the page open for the user to do it manually
@@ -155,9 +183,10 @@ def main():
     # ---> give user time reference while waiting for schedule to open
     golf.timedLaunch()
 
-    input("Press Enter to end program and close browser...")
+    input("Press Enter to end program and close browser pages...")
 
     pageA.closePage()
+    pageB.closePage()
 
 if __name__ == "__main__":
     main()
